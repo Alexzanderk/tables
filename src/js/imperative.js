47347d34aa42658d386b3bce81ec01af9c242692
Alexzanderk <kotsarev.a@gmail.com>
2017-11-12 22:46:11 +0200

    add sort table functionality

--- src/js/imperative.js
+++ src/js/imperative.js
@@ -1,10 +1,5 @@
 const data = require('./../test.json');
-
-// Object.keys(data).forEach(key => data[key].title);
-// const titles = Object.values(data).map(o => o.title);
-// console.log(titles);
-
-
+const dataRow = require('./../test2.json');
 
 export const app = (function () {
 
@@ -34,12 +29,14 @@ export const app = (function () {
     function createTableFrame() {
         if (document.getElementById('table')) return null;
 
-        const jsonTitles = [];
         const fragment = document.createDocumentFragment();
+        const jsonTitles = [];
 
-        Object.keys(data).forEach(key => jsonTitles.push(data[key].title));
-
-        jsonTitles.map(key => fragment.appendChild(createElement('td', { className: 'td' }, key)));
+        Object.keys(data).forEach(key => jsonTitles.push(data[key]));
+        jsonTitles.map(key => {
+            const aLink = createElement('a', { className: 'btnHead headLink', dataset: { 'type': key.type }, href: '#' }, key.title);
+            fragment.appendChild(createElement('th', { className: 'th' }, aLink));
+        });
 
         const row = createElement('tr', { className: 'tr' }, fragment);
         const thead = createElement('thead', { className: 'thead' }, row);
@@ -47,10 +44,10 @@ export const app = (function () {
 
         const table = createElement('table', { className: 'table', id: 'table' }, thead, tbody);
 
-        return tableContainer.appendChild(table);
+        tableContainer.appendChild(table);
 
     }
-
+    /**  uncomment when build
     function addRow() {
         const theadRow = document.querySelector('.thead>.tr');
         const theadCell = theadRow.children;
@@ -73,6 +70,56 @@ export const app = (function () {
         tbody.appendChild(row);
         bindEvents();
     }
+    **/
+    //dev function delete when build
+    function addRow(indexData) {
+        const theadRow = document.querySelector('.thead>.tr');
+        const theadCell = theadRow.children;
+        const cellLength = theadCell.length;
+        const tbody = document.querySelector('.tbody');
+        const fragment = document.createDocumentFragment();
+        const btnControlDelete = createElement('button', { className: 'btn_control btn_delete', dataset: { 'action': 'delete' } }, 'del');
+        const btnControlCopy = createElement('button', { className: 'btn_control btn_copy', dataset: { 'action': 'copy' } }, 'copy');
+        const btnControlPaste = createElement('button', { className: 'btn_control btn_paste', dataset: { 'action': 'paste' } }, 'past');
+        const controllContainer = createElement('div', { className: 'controll_container' }, btnControlDelete, btnControlCopy, btnControlPaste);
+
+        if (indexData == dataRow) {
+            const json = [];   //delete
+            const frag = document.createDocumentFragment();
+
+            Object.keys(indexData).forEach(key => json.push(indexData[key]));
+
+            // console.log(json);
+            json.map(row => {
+                Object.keys(row).forEach(cell => {
+                    const cellInput = createElement('input', { className: 'input', dataset: { 'type': row[cell].type }, type: 'text' });
+                    const cellLabel = createElement('label', { className: 'label', dataset: { 'type': row[cell].type }, }, row[cell].title);
+
+                    frag.appendChild(createElement('td', { className: 'td' }, cellInput, cellLabel));
+                });
+                const btnControlDelete = createElement('button', { className: 'btn_control btn_delete', dataset: { 'action': 'delete' } }, 'del');
+                const btnControlCopy = createElement('button', { className: 'btn_control btn_copy', dataset: { 'action': 'copy' } }, 'copy');
+                const btnControlPaste = createElement('button', { className: 'btn_control btn_paste', dataset: { 'action': 'paste' } }, 'past');
+                const controllContainer = createElement('div', { className: 'controll_container' }, btnControlDelete, btnControlCopy, btnControlPaste);
+                const rows = createElement('tr', { className: 'tr' }, frag, controllContainer);
+                fragment.appendChild(rows);
+            });
+            tbody.appendChild(fragment);
+            bindEvents();
+
+        } else {
+            for (let i = 0; i < cellLength; i++) {
+                const cellInput = createElement('input', { className: 'input', type: 'text' });
+                const cellLabel = createElement('label', { className: 'label' });
+
+                fragment.appendChild(createElement('td', { className: 'td' }, cellInput, cellLabel));
+            };
+
+            const row = createElement('tr', { className: 'tr' }, fragment, controllContainer);
+            tbody.appendChild(row);
+            bindEvents();
+        }
+    }
 
     function removeTable() {
         const table = document.getElementById('table');
@@ -88,7 +135,7 @@ export const app = (function () {
         const editClass = table.querySelectorAll('.td.edit');
         const label = this.querySelector('.label');
         const input = this.querySelector('.input');
-        
+
         const inputKeyListener = (e) => {
             if (e.keyCode === 13) {
                 label.innerText = input.value;
@@ -111,7 +158,7 @@ export const app = (function () {
                 })
                 focusedInput.removeAttribute('id');
             }
-            
+
             editClass.forEach(key => key.classList.remove('edit'));
             input.id = 'focus';
             input.value = label.innerText;
@@ -119,34 +166,161 @@ export const app = (function () {
             label.innerText = input.value;
             focusedInput.removeAttribute('id');
         }
-        
+
         this.classList.toggle('edit');
         input.addEventListener('keydown', inputKeyListener);
         input.focus();
     }
 
-    function rowAction() {
+    function btnRowAction() {
         const dataAction = this.getAttribute('data-action');
         const row = this.parentNode.parentNode;
         const tbody = row.parentNode;
-        
+
         if (dataAction === 'delete') {
             tbody.removeChild(row);
         } else if (dataAction === 'copy') {
             let copyRow;
-            
+
             buffer = document.createDocumentFragment();
             copyRow = row.cloneNode(true);
             buffer.appendChild(copyRow);
         } else if (dataAction === 'paste') {
             const rowNext = row.nextElementSibling;
             let copyBuffer;
-            
+
             copyBuffer = null;
             copyBuffer = buffer.cloneNode(true);
             tbody.insertBefore(copyBuffer, rowNext)
             bindEvents();
         }
+    }
+
+    let dir = 'asc';
+    // function sortTable(e) {
+    //     e.preventDefault();
+
+    //     const findElement = (that, id) => {
+    //         let elem = that.parentNode;
+    //         if (elem.className === id) {
+    //             return elem;
+    //         } else {
+    //             return findElement(elem, id);
+    //         }
+    //     }
+
+    //     const cellIndex = this.parentNode.cellIndex;
+    //     const table = findElement(this, 'table');
+    //     const elementAttribute = this.getAttribute('data-type');
+
+    //     function sortTableGrid(cellIndex, type) {
+    //         const tbody = table.querySelector('tbody');
+    //         const rowsArray = [].slice.call(tbody.rows);
+
+    //         if (dir == 'asc') {
+    //             const compare = function (rowA, rowB) {
+    //                 if (type === 'number') {
+    //                         return +rowA.cells[cellIndex].innerText - +rowB.cells[cellIndex].innerText;
+    //                 } else if (type === 'string') {
+    //                         return rowA.cells[cellIndex].innerText > rowB.cells[cellIndex].innerText;
+    //                 }
+    //             };
+    //             rowsArray.sort(compare);
+    //             dir = 'desc';
+    //         } else if (dir == 'desc') {
+    //             const compare = function (rowA, rowB) {
+    //                 if (type === 'number') {
+    //                     return +rowB.cells[cellIndex].innerText - +rowA.cells[cellIndex].innerText;
+    //                 } else if (type === 'string') {
+    //                     return rowA.cells[cellIndex].innerText < rowB.cells[cellIndex].innerText;
+    //                 }
+    //             };
+    //             rowsArray.sort(compare);
+    //             dir = 'asc';
+    //         }
+
+    //         table.removeChild(tbody);
+
+    //         for (var i = 0; i < rowsArray.length; i++) {
+    //             tbody.appendChild(rowsArray[i]);
+    //         }
+
+    //         table.appendChild(tbody);
+    //     }
+
+    //     sortTableGrid(cellIndex, elementAttribute);
+    // }
+
+    function sortTable(e) {
+        e.preventDefault();
+
+        const findElement = (that, id) => {
+            let elem = that.parentNode;
+            if (elem.className === id) {
+                return elem;
+            } else {
+                return findElement(elem, id);
+            }
+        }
+
+        const cellIndex = this.parentNode.cellIndex;
+        const table = findElement(this, 'table');
+        const elementAttribute = this.getAttribute('data-type');
+        const sortButtons = table.querySelectorAll('th');
+        const sortButton = sortButtons[cellIndex];
+
+        function sortTableGrid(cellIndex, type) {
+            const tbody = table.querySelector('tbody');
+            const rowsArray = [].slice.call(tbody.rows);
+
+
+            if (!sortButton.getAttribute('data-sort')) {
+                sortButtons.forEach(element => element.removeAttribute('data-sort'));
+
+                const compare = function (rowA, rowB) {
+                    if (type === 'number') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'asc');
+                        return +rowA.cells[cellIndex].innerText - +rowB.cells[cellIndex].innerText;
+                    } else if (type === 'string') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'asc');
+                        return rowA.cells[cellIndex].innerHTML > rowB.cells[cellIndex].innerHTML;
+                    }
+                };
+                rowsArray.sort(compare);
+            } else if (sortButton.getAttribute('data-sort') == 'asc') {
+                const compare = function (rowA, rowB) {
+                    if (type === 'number') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'desc');
+                        return +rowB.cells[cellIndex].innerText - +rowA.cells[cellIndex].innerText;
+                    } else if (type === 'string') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'desc');
+                        return rowA.cells[cellIndex].innerHTML < rowB.cells[cellIndex].innerHTML;
+                    }
+                };
+                rowsArray.sort(compare);
+            } else if (sortButton.getAttribute('data-sort') == 'desc') {
+                const compare = function (rowA, rowB) {
+                    if (type === 'number') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'asc');
+                        return +rowA.cells[cellIndex].innerText - +rowB.cells[cellIndex].innerText;
+                    } else if (type === 'string') {
+                        sortButtons[cellIndex].setAttribute('data-sort', 'asc');
+                        return rowA.cells[cellIndex].innerHTML > rowB.cells[cellIndex].innerHTML;
+                    }
+                };
+                rowsArray.sort(compare);
+            }
+
+            table.removeChild(tbody);
+
+            for (var i = 0; i < rowsArray.length; i++) {
+                tbody.appendChild(rowsArray[i]);
+            }
+
+            table.appendChild(tbody);
+        }
+
+        sortTableGrid(cellIndex, elementAttribute);
     }
 
     // cash DOM
@@ -163,15 +337,22 @@ export const app = (function () {
     function bindEvents() {
         const cells = document.querySelectorAll('tbody>tr>.td');
         const controllContainer = document.querySelectorAll('.btn_control');
-        
+        const sortCell = document.querySelectorAll('.thead>.tr>.th>.headLink');
+
         cells.forEach(key => key.addEventListener('dblclick', writeCell, false));
-        controllContainer.forEach(key => key.addEventListener('click', rowAction));
+        controllContainer.forEach(key => key.addEventListener('click', btnRowAction));
+        sortCell.forEach(key => key.addEventListener('click', sortTable));
     }
 
 
     createTableFrame(); //delete when build
-    addRow();//delete when build
+    addRow(dataRow);//delete when build
+    addRow(dataRow);//delete when build
 
 
 
 })();
+
+
+
+
